ehem/freebsd-src

intrng: remove architecture copy of PIC device
Now that the value is on the event, there is no need for INTRNG to
to duplicate the value on the intr_irqsrc structure.

sys/kern/subr_intr.c
@@ -407,7 +407,7 @@ intr_isrc_dispatch(struct intr_irqsrc *isrc, struct trapframe *tf)
 	if (isrc->isrc_filter != NULL) {
 		int error;
 		error = isrc->isrc_filter(isrc->isrc_arg, tf);
-		PIC_POST_FILTER(isrc->isrc_dev, isrc);
+		PIC_POST_FILTER(isrc->isrc_event.ie_pic, isrc);
 		if (error == FILTER_HANDLED)
 			return (0);
 	} else
@@ -431,7 +431,7 @@ intr_isrc_dispatch(struct intr_irqsrc *isrc, struct trapframe *tf)
  *     constantly...
  */
 static inline int
-isrc_alloc_irq(struct intr_irqsrc *isrc)
+isrc_alloc_irq(struct intr_irqsrc *isrc, device_t dev)
 {
 	u_int irq;
 	int error;
@@ -454,7 +454,7 @@ isrc_alloc_irq(struct intr_irqsrc *isrc)
 	return (ENOSPC);
 
 found:
-	error = intr_event_init(&isrc->isrc_event, isrc->isrc_dev,
+	error = intr_event_init(&isrc->isrc_event, dev,
 	    isrc->isrc_irq, 0, "%s:", isrc->isrc_name);
 	if (error != 0)
 		return (error);
@@ -516,7 +516,6 @@ intr_isrc_register(struct intr_irqsrc *isrc, device_t dev, u_int flags,
 	va_list ap;
 
 	bzero(isrc, sizeof(struct intr_irqsrc));
-	isrc->isrc_dev = dev;
 	isrc->isrc_irq = INTR_IRQ_INVALID;	/* just to be safe */
 	isrc->isrc_flags = flags;
 
@@ -525,7 +524,7 @@ intr_isrc_register(struct intr_irqsrc *isrc, device_t dev, u_int flags,
 	va_end(ap);
 
 	mtx_lock(&isrc_table_lock);
-	error = isrc_alloc_irq(isrc);
+	error = isrc_alloc_irq(isrc, dev);
 	if (error != 0) {
 		mtx_unlock(&isrc_table_lock);
 		return (error);
@@ -976,7 +975,7 @@ intr_activate_irq(device_t dev, struct resource *res)
 	}
 	intr_map_set_isrc(res_id, isrc);
 	rman_set_virtual(res, data);
-	return (PIC_ACTIVATE_INTR(isrc->isrc_dev, isrc, res, data));
+	return (PIC_ACTIVATE_INTR(isrc->isrc_event.ie_pic, isrc, res, data));
 }
 
 int
@@ -997,7 +996,7 @@ intr_deactivate_irq(device_t dev, struct resource *res)
 		    res_id);
 
 	data = rman_get_virtual(res);
-	error = PIC_DEACTIVATE_INTR(isrc->isrc_dev, isrc, res, data);
+	error = PIC_DEACTIVATE_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 	intr_map_set_isrc(res_id, NULL);
 	rman_set_virtual(res, NULL);
 	free(data, M_INTRNG);
@@ -1059,11 +1058,11 @@ intr_setup_irq(device_t dev, struct resource *res, driver_filter_t filt,
 		return (error);
 
 	mtx_lock(&isrc_table_lock);
-	error = PIC_SETUP_INTR(isrc->isrc_dev, isrc, res, data);
+	error = PIC_SETUP_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 	if (error == 0) {
 		isrc->isrc_handlers++;
 		if (isrc->isrc_handlers == 1)
-			PIC_ENABLE(isrc->isrc_dev, isrc);
+			PIC_ENABLE(isrc->isrc_event.ie_pic, isrc);
 	}
 	mtx_unlock(&isrc_table_lock);
 	if (error != 0)
@@ -1098,8 +1097,8 @@ intr_teardown_irq(device_t dev, struct resource *res, void *cookie)
 		isrc->isrc_filter = NULL;
 		isrc->isrc_arg = NULL;
 		isrc->isrc_handlers = 0;
-		PIC_DISABLE(isrc->isrc_dev, isrc);
-		PIC_TEARDOWN_INTR(isrc->isrc_dev, isrc, res, data);
+		PIC_DISABLE(isrc->isrc_event.ie_pic, isrc);
+		PIC_TEARDOWN_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 		isrc_update_name(isrc, NULL);
 		mtx_unlock(&isrc_table_lock);
 		return (0);
@@ -1113,8 +1112,8 @@ intr_teardown_irq(device_t dev, struct resource *res, void *cookie)
 		mtx_lock(&isrc_table_lock);
 		isrc->isrc_handlers--;
 		if (isrc->isrc_handlers == 0)
-			PIC_DISABLE(isrc->isrc_dev, isrc);
-		PIC_TEARDOWN_INTR(isrc->isrc_dev, isrc, res, data);
+			PIC_DISABLE(isrc->isrc_event.ie_pic, isrc);
+		PIC_TEARDOWN_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 		intrcnt_updatename(isrc);
 		mtx_unlock(&isrc_table_lock);
 	}
@@ -1238,7 +1237,7 @@ intr_irq_shuffle(void *arg __unused)
 		 * for bound ISRC. The best thing we can do is to clear
 		 * isrc_cpu so inconsistency with ie_cpu will be detectable.
 		 */
-		if (PIC_BIND_INTR(isrc->isrc_dev, isrc) != 0)
+		if (PIC_BIND_INTR(isrc->isrc_event.ie_pic, isrc) != 0)
 			CPU_ZERO(&isrc->isrc_cpu);
 	}
 	mtx_unlock(&isrc_table_lock);

DeepCoreDK/FreeBSD

sys/kern/subr_intr.c
@@ -398,7 +398,7 @@ intr_isrc_dispatch(struct intr_irqsrc *isrc, struct trapframe *tf)
 	if (isrc->isrc_filter != NULL) {
 		int error;
 		error = isrc->isrc_filter(isrc->isrc_arg, tf);
-		PIC_POST_FILTER(isrc->isrc_dev, isrc);
+		PIC_POST_FILTER(isrc->isrc_event.ie_pic, isrc);
 		if (error == FILTER_HANDLED)
 			return (0);
 	} else
@@ -422,7 +422,7 @@ intr_isrc_dispatch(struct intr_irqsrc *isrc, struct trapframe *tf)
  *     constantly...
  */
 static inline int
-isrc_alloc_irq(struct intr_irqsrc *isrc)
+isrc_alloc_irq(struct intr_irqsrc *isrc, device_t dev)
 {
 	u_int irq;
 	int error;
@@ -445,7 +445,7 @@ isrc_alloc_irq(struct intr_irqsrc *isrc)
 	return (ENOSPC);
 
 found:
-	error = intr_event_init(&isrc->isrc_event, isrc->isrc_dev, irq, 0,
+	error = intr_event_init(&isrc->isrc_event, dev, irq, 0,
 	    "%s:", isrc->isrc_name);
 	if (error != 0)
 		return (error);
@@ -507,7 +507,6 @@ intr_isrc_register(struct intr_irqsrc *isrc, device_t dev, u_int flags,
 	va_list ap;
 
 	bzero(isrc, sizeof(struct intr_irqsrc));
-	isrc->isrc_dev = dev;
 	isrc->isrc_irq = INTR_IRQ_INVALID;	/* just to be safe */
 	isrc->isrc_flags = flags;
 
@@ -516,7 +515,7 @@ intr_isrc_register(struct intr_irqsrc *isrc, device_t dev, u_int flags,
 	va_end(ap);
 
 	mtx_lock(&isrc_table_lock);
-	error = isrc_alloc_irq(isrc);
+	error = isrc_alloc_irq(isrc, dev);
 	if (error != 0) {
 		mtx_unlock(&isrc_table_lock);
 		return (error);
@@ -634,7 +633,7 @@ intr_isrc_assign_cpu(device_t pic, interrupt_t *isrc, u_int cpu)
 	 * informed if the call is successful.
 	 */
 	if (irq_assign_cpu) {
-		error = PIC_BIND_INTR(isrc->isrc_dev, isrc);
+		error = PIC_BIND_INTR(isrc->isrc_event.ie_pic, isrc);
 		if (error) {
 			CPU_ZERO(&isrc->isrc_cpu);
 			mtx_unlock(&isrc_table_lock);
@@ -968,7 +967,7 @@ intr_activate_irq(device_t dev, struct resource *res)
 	}
 	intr_map_set_isrc(res_id, isrc);
 	rman_set_virtual(res, data);
-	return (PIC_ACTIVATE_INTR(isrc->isrc_dev, isrc, res, data));
+	return (PIC_ACTIVATE_INTR(isrc->isrc_event.ie_pic, isrc, res, data));
 }
 
 int
@@ -989,7 +988,7 @@ intr_deactivate_irq(device_t dev, struct resource *res)
 		    res_id);
 
 	data = rman_get_virtual(res);
-	error = PIC_DEACTIVATE_INTR(isrc->isrc_dev, isrc, res, data);
+	error = PIC_DEACTIVATE_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 	intr_map_set_isrc(res_id, NULL);
 	rman_set_virtual(res, NULL);
 	free(data, M_INTRNG);
@@ -1051,11 +1050,11 @@ intr_setup_irq(device_t dev, struct resource *res, driver_filter_t filt,
 		return (error);
 
 	mtx_lock(&isrc_table_lock);
-	error = PIC_SETUP_INTR(isrc->isrc_dev, isrc, res, data);
+	error = PIC_SETUP_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 	if (error == 0) {
 		isrc->isrc_handlers++;
 		if (isrc->isrc_handlers == 1)
-			PIC_ENABLE_INTR(isrc->isrc_dev, isrc);
+			PIC_ENABLE_INTR(isrc->isrc_event.ie_pic, isrc);
 	}
 	mtx_unlock(&isrc_table_lock);
 	if (error != 0)
@@ -1090,8 +1089,8 @@ intr_teardown_irq(device_t dev, struct resource *res, void *cookie)
 		isrc->isrc_filter = NULL;
 		isrc->isrc_arg = NULL;
 		isrc->isrc_handlers = 0;
-		PIC_DISABLE_INTR(isrc->isrc_dev, isrc);
-		PIC_TEARDOWN_INTR(isrc->isrc_dev, isrc, res, data);
+		PIC_DISABLE_INTR(isrc->isrc_event.ie_pic, isrc);
+		PIC_TEARDOWN_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 		isrc_update_name(isrc, NULL);
 		mtx_unlock(&isrc_table_lock);
 		return (0);
@@ -1105,8 +1104,8 @@ intr_teardown_irq(device_t dev, struct resource *res, void *cookie)
 		mtx_lock(&isrc_table_lock);
 		isrc->isrc_handlers--;
 		if (isrc->isrc_handlers == 0)
-			PIC_DISABLE_INTR(isrc->isrc_dev, isrc);
-		PIC_TEARDOWN_INTR(isrc->isrc_dev, isrc, res, data);
+			PIC_DISABLE_INTR(isrc->isrc_event.ie_pic, isrc);
+		PIC_TEARDOWN_INTR(isrc->isrc_event.ie_pic, isrc, res, data);
 		intrcnt_updatename(isrc);
 		mtx_unlock(&isrc_table_lock);
 	}
@@ -1230,7 +1229,7 @@ intr_irq_shuffle(void *arg __unused)
 		 * for bound ISRC. The best thing we can do is to clear
 		 * isrc_cpu so inconsistency with ie_cpu will be detectable.
 		 */
-		if (PIC_BIND_INTR(isrc->isrc_dev, isrc) != 0)
+		if (PIC_BIND_INTR(isrc->isrc_event.ie_pic, isrc) != 0)
 			CPU_ZERO(&isrc->isrc_cpu);
 	}
 	mtx_unlock(&isrc_table_lock);